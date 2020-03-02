05f3ffb7c17235903ac1a9524e789b6ce9b704d1
face2ram <bgsanap@gmail.com>
2020-03-02 07:51:46 +0530

    Updates

---
+++ precache-manifest.848520a19111693f4b4b167c22eef536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f8140d87549a709c9f7c46f99dd35e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "003d4158faa96741152a",
+    "url": "/monsters-rolodex/static/css/main.ecf6be3d.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003d4158faa96741152a",
+    "url": "/monsters-rolodex/static/js/main.f52fe230.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
